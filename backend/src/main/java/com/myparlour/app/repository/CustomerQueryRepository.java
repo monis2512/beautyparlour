eb1cacda1e6d30a47c74f1337035534f29b9a620
monis2512 <102420522+monis2512@users.noreply.github.com>
2026-09-24 21:29:54 +0530

    Create CustomerQueryRepository.java

---
+++ backend/src/main/java/com/myparlour/app/repository/CustomerQueryRepository.java
@@ -0,0 +1,5 @@
+package com.myparlour.app.repository;
+import com.myparlour.app.entity.CustomerQuery;
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface CustomerQueryRepository extends JpaRepository<CustomerQuery,Long>{}
+
