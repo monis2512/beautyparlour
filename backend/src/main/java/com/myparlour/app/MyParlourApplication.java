92f03d9ab7b8e25fa354ead06b2d8c559a4c22f5
monis2512 <102420522+monis2512@users.noreply.github.com>
2026-09-24 21:24:21 +0530

    Create MyParlourApplication.java

---
+++ backend/src/main/java/com/myparlour/app/MyParlourApplication.java
@@ -0,0 +1,6 @@
+package com.myparlour.app;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class MyParlourApplication { public static void main(String[] args){ SpringApplication.run(MyParlourApplication.class,args); } }
+
